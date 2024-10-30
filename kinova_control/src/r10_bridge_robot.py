19c939b6938d80acd87be8fc62b570ab59ee83f1
izenderi <ziliang1121@gmail.com>
2024-10-30 16:26:43 -0700

    add feedback

--- kinova_control/src/r10_bridge_robot.py
+++ kinova_control/src/r10_bridge_robot.py
@@ -4,26 +4,34 @@
 from __future__ import print_function
 import roslibpy
 import json
+from threading import Thread
 
 import rospy
 
 import time
 
+import socket
+
 sum_network_latency = 0.0
 avg_network_latency = 0.0
 count = 0
+
+data_string = ''
+time1 = 0.0
 
 def parse_message(message):
   global count
   global sum_network_latency
   global avg_network_latency
+  global data_string
+  global time1
 
   listener_start = time.time()
 
   # Parse the JSON data string
   parsed_message = json.loads(message)
   
-  # Extract the vaalues
+  # Extract the values
   data_string = parsed_message['fused_pose']
   time1 = parsed_message['time1']
 
@@ -63,14 +71,44 @@ def listen_from_xr(xr, port):
   except KeyboardInterrupt:
       client.terminate()
 
-if __name__ == '__main__':
-  try:
-    rospy.init_node('rosbridge_robot')
+def robot_to_xr(robot, port, xr, period=1):
+    global time1
+    global data_string
 
-    # host = "10.13.146.101"  # Replace with server IP address if needed
-    host = "localhost"  # Replace with server IP address if needed
+    # Function to send data back to xr_bridge
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((xr, port))  # Ensure it matches xr_bridge's listen port
+        while True:
+            data = f"{str(time1)}:{str(data_string)}"
+
+            #==================<RTEN>======================
+            time.sleep(0.2) # Maunal delay mimic single-way from robot to xr
+            #==================<RTEN/>======================
+
+            s.sendall(data.encode())
+            time.sleep(period)
+
+# if __name__ == '__main__':
+#   try:
+#     rospy.init_node('rosbridge_robot')
+
+#     # host = "10.13.146.101"  # Replace with server IP address if needed
+#     host = "localhost"  # Replace with server IP address if needed
+#     port = 9090
+#     listen_from_xr(host, port)
+#   except rospy.ROSInterruptException:
+#     print("program interrupted before completion")
+
+if __name__ == '__main__':
+    rospy.init_node('rosbridge_robot')
+    host = "localhost"
     port = 9090
-    # receive_data(host, port)
-    listen_from_xr(host, port)
-  except rospy.ROSInterruptException:
-    print("program interrupted before completion")
+
+    listen_thread = Thread(target=listen_from_xr, args=(host, port))
+    send_thread = Thread(target=robot_to_xr, args=("", 9091, "localhost", 0.5))
+
+    listen_thread.start()
+    send_thread.start()
+
+    listen_thread.join()
+    send_thread.join()
